917dc19ae3eb52df111cad6a45b4872c023bc3a4
UPye <Urika.Pye@gmail.com>
2021-06-09 11:22:01 -0400

    Got commercial image rendering.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
   const currentCategory = {
@@ -11,8 +12,12 @@ function Gallery(props) {
     <section>
       <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.name}</p>
+      <div>
+          <img
+            src={photo}
+          />
+      </div>
     </section>
   );
 }
-
 export default Gallery;
